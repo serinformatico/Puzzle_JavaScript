3ecafdd4a231a6711048ed9853bdd905bf8cbf00
Sergio Regalado Alessi <serdigital@live.com.ar>
2017-12-20 02:38:09 -0300

    Commit Nº5: Filtro de elementos

--- js/mi_app.js
+++ js/mi_app.js
@@ -3,15 +3,13 @@
     crearFiltros();
     inicializarSlider();
 		$("#btnMostrarTodos").click(function(){
-			mostrarElementos();
+			mostrarElementosTodos();
 		});
-    $("#selectCiudad").change(function(){
-      filtrarElementos($("#selectCiudad").val(), $("#selectTipo").val());
-    });
-    $("#selectTipo").change(function(){
-      filtrarElementos($("#selectCiudad").val(), $("#selectTipo").val());
+    $("#submitButton").click(function(){
+      filtrarElementos();
     });
 	});
+
 
 
   function crearFiltros(){
@@ -33,13 +31,45 @@
     }});    
   }
 
-  function filtrarElementos(filtro1, filtro2){
-    alert(filtro1+filtro2);
-  
+
+  function filtrarElementos(){
+    var filtroCiudad = $("#selectCiudad").val();
+    var filtroTipo = $("#selectTipo").val();
+    var filtroPrecioMin = $("#rangoPrecio").val().substring(0, $("#rangoPrecio").val().indexOf(';'));
+    var filtroPrecioMax = $("#rangoPrecio").val().substring($("#rangoPrecio").val().indexOf(';')+1, $("#rangoPrecio").val().length);
+
+    $("#elementosEncontrados").empty();
+    $.ajax({url: "data-1.json", success: function(datos){
+      $.each(datos, function(indice, elemento){
+
+          var precioFloat = parseFloat(elemento.Precio.substring(1, elemento.Precio.length).replace(",", "."));
+          if(elemento.Ciudad == filtroCiudad && elemento.Tipo == filtroTipo){
+            if(precioFloat>=filtroPrecioMin && precioFloat<=filtroPrecioMax){
+              var resultado = '';
+              resultado += '<div id="elemento'+elemento.Id+'" class="tituloContenido card">';
+              resultado +=  '<div class="col s4">';
+              resultado +=    '<img src="img/home.jpg" class="col s12">';
+              resultado +=  '</div>';
+              resultado +=  '<div class="col s8">';
+              resultado +=    '<sapn class="card-content"><b>Direccion:</b> '+elemento.Direccion+'</span><br>';
+              resultado +=    '<sapn class="card-content"><b>Ciudad:</b> '+elemento.Ciudad+'</span><br>';
+              resultado +=    '<sapn class="card-content"><b>Telefono:</b> '+elemento.Telefono+'</span><br>';
+              resultado +=    '<sapn class="card-content"><b>Codigo Postal:</b> '+elemento.Codigo_Postal+'</span><br>';
+              resultado +=    '<sapn class="card-content"><b>Tipo:</b> '+elemento.Tipo+'</span><br>';
+              resultado +=    '<sapn class="card-content"><b>Precio:</b> '+elemento.Precio+'</span><br>';
+              resultado +=  '</div>';
+              resultado += '</div>';
+              $("#elementosEncontrados").append(resultado);  
+            }
+          }
+      });
+    }});  
   }
 
 
-  function mostrarElementos(filtro1, filtro2){
+
+  function mostrarElementosTodos(){
+    $("#elementosEncontrados").empty();
     $.ajax({url: "data-1.json", success: function(datos){
       $.each(datos, function(indice, elemento){
           var resultado = '';
@@ -58,19 +88,23 @@
           resultado += '</div>';
           $("#elementosEncontrados").append(resultado);
       });
+      //Reseteo Cajas Select
       $("#selectCiudad").val(null);
       $("#selectTipo").val(null);
     }});    
   }
 
+
+  //En esta función realice una modificación debido a que el rango de valores era demasiado alto en realación
+  //  preciolaoss de las propiedades(casas) especificados en el archivo data-1.json
   function inicializarSlider(){
     $("#rangoPrecio").ionRangeSlider({
-      type: "double",
+      type: "float",
       grid: false,
       min: 0,
-      max: 100000,
-      from: 200,
-      to: 80000,
+      max: 100,
+      from: 20,
+      to: 80,
       prefix: "$"
     });
   }
